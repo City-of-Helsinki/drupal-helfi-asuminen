d3e348ab67d8ec74fa686b3fb5745658a15ce0e4
tuutti <tuutti@iki.fi>
2021-11-10 11:44:01 +0200

    Enable crons

--- docker/openshift/crons/base.sh
+++ docker/openshift/crons/base.sh
@@ -19,9 +19,9 @@ echo "Starting cron: $(date)"
 # Uncomment this to enable TPR migration cron
 #exec "/crons/migrate-tpr.sh" &
 # Uncomment this to enable Varnish purge cron
-#exec "/crons/purge-queue.sh" &
+xec "/crons/purge-queue.sh" &
 # Uncomment this to enable automatic translation updates.
-# exec "/crons/update-translations.sh" &
+exec "/crons/update-translations.sh" &
 
 while true
 do
