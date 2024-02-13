63262f884ff738543bdac5d457d08e68576e20f0
tuutti <tuutti@iki.fi>
2024-02-13 11:02:21 +0200

    UHF-9546: Fixes

--- public/sites/default/testing.settings.php
+++ public/sites/default/testing.settings.php
@@ -1,3 +1,11 @@
 <?php
 
 $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/asuminen/openid-connect/tunnistamo';
+$config['helfi_proxy.settings']['asset_path'] = 'asuminen-assets';
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'housing',
+  'fi' => 'asuminen',
+  'sv' => 'boende',
+  'ru' => 'housing',
+  'zxx' => 'housing',
+];
